da423838edacbee48e07d8c87c739da1523e2564
Anthony Albertorio <anthony.albertorio@gmail.com>
2017-01-14 20:47:08 -0500

    front end implementation
    Front end implementation based on draft kings website. I will mod it so
    that it fits our needs.

---
+++ views/js/animsition-effects.js
@@ -0,0 +1,15 @@
+$(".animsition").animsition({
+  inClass: 'fade-in-left-sm',
+  outClass: 'fade-out-left-sm',
+  linkElement: 'navbar navbar-default a',
+  inDuration: 1500,
+  outDuration: 500
+});
+
+$(".jumbotron animsition").animsition({
+  inClass: 'rotate-in-lg',
+  outClass: 'rotate-out-lg',
+  inDuration: 1500,
+  outDuration: 500
+});
+
---
+++ views/js/app.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+    alert('app.js loaded');
+
+    // add drag and drop to the politican app.
+	$('.politician').css('background-color', 'white');
+});
